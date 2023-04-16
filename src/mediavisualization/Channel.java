b66dd42141042f01d26c70a7da6262a179b042a3
Austin Zary <austinz21@vt.edu>
2023-04-16 16:10:21 -0400

    Added Channel's getters

--- src/mediavisualization/Channel.java
+++ src/mediavisualization/Channel.java
@@ -5,7 +5,7 @@ package mediavisualization;
  * to store the engagement objects.
  */
 public class Channel {
-    
+
     private Month month;
     private String username;
     private String channelName;
@@ -17,16 +17,16 @@ public class Channel {
     private int comments;
     private int views;
 
-    public Channel(Month month, 
-    String username, 
-    String channelName, 
-    String country, 
-    String mainTopic, 
-    int likes, 
-    int posts, 
-    int followers, 
-    int comments, 
-    int views) {
+    public Channel(Month month,
+            String username,
+            String channelName,
+            String country,
+            String mainTopic,
+            int likes,
+            int posts,
+            int followers,
+            int comments,
+            int views) {
         this.month = month;
         this.username = username;
         this.channelName = channelName;
@@ -37,5 +37,68 @@ public class Channel {
         this.followers = followers;
         this.comments = comments;
         this.views = views;
+    }
+
+    /**
+     * 
+     * @return
+     *         The month associated with this channel.
+     */
+    public Month getMonth() {
+        return month;
+    }
+
+    /**
+     * 
+     * @return
+     *         The channel's name.
+     */
+    public String getChannelName() {
+        return channelName;
+    }
+
+    /**
+     * 
+     * @return
+     *         The number of likes for this channel in the stored month.
+     */
+    public int getLikes() {
+        return likes;
+    }
+
+    /**
+     * 
+     * @return
+     *         The number of posts for this channel in the stored month.
+     */
+    public int getPosts() {
+        return posts;
+    }
+
+    /**
+     * 
+     * @return
+     *         The number of followers for this channel in the stored month.
+     */
+    public int getFollowers() {
+        return followers;
+    }
+
+    /**
+     * 
+     * @return
+     *         The number of comments for this channel in the stored month.
+     */
+    public int getComments() {
+        return comments;
+    }
+
+    /**
+     * 
+     * @return
+     *         The number of views for this channel in the stored month.
+     */
+    public int getViews() {
+        return views;
     }
 }
