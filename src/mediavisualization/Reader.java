b2cdd2cb13ea83ebcd6cc9d92c3a1f79d503a571
Austin Zary <austinz21@vt.edu>
2023-04-17 11:16:31 -0400

    Continued work on Reader

--- src/mediavisualization/Reader.java
+++ src/mediavisualization/Reader.java
@@ -13,43 +13,93 @@ public class Reader {
         readFile(fileName);
     }
 
+    /**
+     * Extracts the data from each line of the provided file. Then, creates a new
+     * Channel object containing that data and adds it to the linked list containing
+     * all channel objects.
+     * 
+     * @param fileName
+     *                 The name of the file containing the channel data to be read.
+     * @throws FileNotFoundException
+     */
     public void readFile(String fileName) throws FileNotFoundException {
         File file = new File(fileName);
         Scanner scanner = new Scanner(file);
+        scanner.nextLine(); // Skip the line containing headers
+
         while (scanner.hasNextLine()) {
             String[] arr = scanner.nextLine().split(",\\s*");
+
             Month month;
+            String username;
+            String channelName;
+            String country;
+            String mainTopic;
+            int likes;
+            int posts;
+            int followers;
+            int comments;
+            int views;
+
             switch (arr[0]) {
                 case "January":
                     month = Month.JAN;
+                    break;
                 case "Febraury":
                     month = Month.FEB;
+                    break;
                 case "March":
                     month = Month.MAR;
+                    break;
                 case "April":
                     month = Month.APR;
+                    break;
                 case "May":
                     month = Month.MAY;
+                    break;
                 case "June":
                     month = Month.JUN;
+                    break;
                 case "July":
                     month = Month.JUL;
+                    break;
                 case "August":
                     month = Month.AUG;
+                    break;
                 case "September":
                     month = Month.SEP;
+                    break;
                 case "October":
                     month = Month.OCT;
+                    break;
                 case "November":
                     month = Month.NOV;
+                    break;
                 case "December":
                     month = Month.DEC;
+                    break;
                 default:
-                    month = null;
+                    month = null; // Should handle ignoring invalid months in conjunction with if statement below.
+                    break;
             }
 
             if (month != null) {
-                //TODO Finish implementing the reader here.
+                username = arr[1];
+                channelName = arr[2];
+                if (arr.length == 10) {
+                    country = arr[3];
+                } else {
+                    country = "";
+                }
+                mainTopic = arr[arr.length - 6];
+                likes = Integer.valueOf(arr[arr.length - 5]);
+                posts = Integer.valueOf(arr[arr.length - 4]);
+                followers = Integer.valueOf(arr[arr.length - 3]);
+                comments = Integer.valueOf(arr[arr.length - 2]);
+                views = Integer.valueOf(arr[arr.length - 1]);
+
+                // TODO When list is implemented, add channel to list.
+                new Channel(month, username, channelName, country, mainTopic, likes, posts, followers, comments, views);
             }
         }
         scanner.close();
