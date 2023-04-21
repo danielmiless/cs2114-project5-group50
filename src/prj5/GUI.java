06910df76cc980c0a2a616371a859108ee5dddd5
Richard Nguyen <rtrichardnguyen@gmail.com>
2023-04-21 08:50:58 -0400

    GUI private fields

--- src/prj5/GUI.java
+++ src/prj5/GUI.java
@@ -1,5 +1,13 @@
 package prj5;
 
+import cs2.*;
+import cs2.Button;
+import cs2.Shape;
+import cs2.Window;
+import list.AList;
+
+import java.awt.*;
+
 /**
  * The GUI class contains all graphical user
  * interface elements and processes.
@@ -7,5 +15,16 @@ package prj5;
  * @version 2023.04.21
  */
 public class GUI {
-    
+    private Window window;
+    private Reader reader;
+    private Button quit;
+    private Button sortEngagement;
+    private Button sortChannelName;
+    private Button traditionalEngagement;
+    private Button reachEngagement;
+    private Button january;
+    private Button february;
+    private Button march;
+    private Button firstQuarter;
+
 }
