4e0e0f7f022f1fe94cab32c2983ca02e1946f192
Austin Zary <austinz21@vt.edu>
2023-04-20 23:13:58 -0400

    Deleted unusued classes

--- src/prj5/Month.java
+++
@@ -1,8 +0,0 @@
-package prj5;
-
-/**
- * 
- */
-public enum Month {
-    JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC;
-}
--- src/prj5/Sorting.java
+++
@@ -1,9 +0,0 @@
-package prj5;
-
-/**
- * The sorting class is used to sort the channels in various ways.
- */
-public class Sorting {
-
-
-}
