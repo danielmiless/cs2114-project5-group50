f93bd3568bf5aedfe44b64afe5a5148b7a87a44f
Richard Nguyen <rtrichardnguyen@gmail.com>
2023-04-28 21:19:58 -0400

    javadoc

--- src/prj5/Sort.java
+++ src/prj5/Sort.java
@@ -1,5 +1,17 @@
 package prj5;
 
+/**
+ * sort Enum
+ * @author Austin Zary, Richard Nguyen, Daniel Miles
+ * @version 2023.04.28
+ */
 public enum Sort {
+
+    /**
+     * @field TRADITIONAL sort type
+     * @field REACH sort type
+     * @field NAME sort type
+     * @field ENGAGEMENT sort type
+     */
     TRADITIONAL, REACH, NAME, ENGAGEMENT;
 }
